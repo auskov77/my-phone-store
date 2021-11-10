12d416704707146de33eed5f440a24f9f5d7aa73
auskov77 <auskov77@yandex.ru>
2021-11-10 14:13:41 +0300

    version 3

--- src/test/java/ru/itsjava/service/ClientServiceImplTest.java
+++ src/test/java/ru/itsjava/service/ClientServiceImplTest.java
@@ -9,14 +9,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClientServiceImplTest {
-    public static final String DEFOULT_NAME = "Honor"; // создаем параметр DEFAULT_NAME
+    public static final String DEFAULT_NAME = "Honor"; // создаем параметр DEFAULT_NAME
     public static final int DEFAULT_PRICE = 25_000; // создаем параметр DEFAULT_PRICE
     List<Phone> phoneList = new ArrayList<>(); // создаем коллекцию phoneList
 
     @DisplayName("Проверка метода buyPhone")
     @Test
     public void checkBuyPhoneMethod() {
-        Phone actualPhone = new Phone(DEFOULT_NAME, DEFAULT_PRICE); // создаем объект actualPhone с переменными (DEFAULT_NAME, DEFAULT_PRICE)
+        Phone actualPhone = new Phone(DEFAULT_NAME, DEFAULT_PRICE); // создаем объект actualPhone с переменными (DEFAULT_NAME, DEFAULT_PRICE)
         phoneList.add(actualPhone);  // в коллекцию phoneList добавили объект actualPhone
         Assertions.assertTrue(phoneList.contains(actualPhone)); // проверяем что передаваемое выраежение истинно: т.е. в коллекции phoneList содержится объект actualPhone
     }
@@ -24,7 +24,7 @@ public class ClientServiceImplTest {
     @DisplayName("Проверка метода putPhone")
     @Test
     public void checkPutPhoneMethod() {
-        Phone actualPhone = new Phone(DEFOULT_NAME, DEFAULT_PRICE); // создаем объект actualPhone с переменными (DEFAULT_NAME, DEFAULT_PRICE)
+        Phone actualPhone = new Phone(DEFAULT_NAME, DEFAULT_PRICE); // создаем объект actualPhone с переменными (DEFAULT_NAME, DEFAULT_PRICE)
         phoneList.add(actualPhone); // в коллекцию phoneList добавили объект actualPhone
         Assertions.assertTrue(phoneList.contains(actualPhone)); // проверяем что передаваемое выраежение истинно: т.е. в коллекции phoneList содержится объект actualPhone
     }
@@ -32,7 +32,7 @@ public class ClientServiceImplTest {
     @DisplayName("Проверка метода printPhones")
     @Test
     public void checkPrintPhoneMethod() {
-        Phone actualPhone = new Phone(DEFOULT_NAME, DEFAULT_PRICE); // создаем объект actualPhone с переменными (DEFAULT_NAME, DEFAULT_PRICE)
+        Phone actualPhone = new Phone(DEFAULT_NAME, DEFAULT_PRICE); // создаем объект actualPhone с переменными (DEFAULT_NAME, DEFAULT_PRICE)
         phoneList.add(actualPhone); // в коллекцию phoneList добавили объект actualPhone
         Assertions.assertEquals(actualPhone, phoneList.get(0)); // проверяем равенство двух обхектов: ожидание - actualPhone, фактический результат - получить позицию 0 коллекции phoneList
     }
